7ed6c9b0d27e6eb47ba26766fd6a87a1b2cacfb7
Sergei Afanasev <afanasev@ntpayments.com>
2024-12-20 11:15:03 +0400

    Added output null

--- Xray/installation/uninstall.sh
+++ Xray/installation/uninstall.sh
@@ -4,13 +4,13 @@
 sudo systemctl stop xray
 
 # Remove the Xray binary
-sudo rm /usr/local/bin/xray
+sudo rm /usr/local/bin/xray 2> /dev/null
 
 # Remove the Xray configuration file
-sudo rm /usr/local/etc/xray/config.json
+sudo rm /usr/local/etc/xray/config.json 2> /dev/null
 
 # Remove the Xray service file
-sudo rm /etc/systemd/system/xray.service
+sudo rm /etc/systemd/system/xray.service 2> /dev/null
 
 # Reload the systemd daemon
 sudo systemctl daemon-reload
@@ -19,4 +19,4 @@ sudo systemctl daemon-reload
 sudo rm -rf /var/log/xray
 
 # Remove job in cron
-sed -i '/Xray/d' /etc/crontab
+sudo sed -i '/Xray/d' /etc/crontab
